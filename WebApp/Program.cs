2cb3729d38414a0e4e7420f08e25fcaf4aa533ba
Jojaah12 <david.aljojah@gmail.com>
2024-04-11 15:46:34 +0200

    Created controlles folder

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -5,11 +5,17 @@ namespace WebApp
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.UseRouting();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
-        }
+        } 
     }
 }
